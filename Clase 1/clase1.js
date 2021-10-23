84cceda2e7467b5b199126521ca04e89f0766fc7
Juan Pablo Medina <juanpablomedina1997@gmail.com>
2021-10-23 14:33:09 -0300

    JavaScript 1.0

---
+++ Clase 1/clase1.js
@@ -0,0 +1,22 @@
+let nombre, apellido, edad;
+
+nombre = prompt("Ingrese su nombre:");
+alert("Hola " + nombre);
+apellido = prompt("Ingrese su apellido:");
+edad = prompt("Ingrese su edad:");
+
+
+console.log("Su nombre y apellido es " + nombre + " " + apellido + " y su edad " + edad);
+
+
+let num1, num2, resultado;
+
+num1 = parseInt(prompt("Ingrese el primer numero"));
+num2= parseInt(prompt("Ingrese el segundo numero"));
+resultado = num1 + num2;
+console.log("La suma de los dos numeros es: " + resultado);
+
+
+let texto1 = prompt("Ingrese su nombre");
+let texto2 = prompt("Ingrese su apellido");
+let texto3 = alert(texto1 + texto2);
---
+++ Clase 2/clase2Medina.js
@@ -0,0 +1,12 @@
+let num1 = parseInt(prompt("Ingrese un numero:"));
+
+if (num1 > 1000) alert("Es mayor que 1000");
+
+
+let texto1 = prompt("Ingrese un texto, si es 'Hola' sale un alert");
+
+if (texto1 == "Hola") alert("Escribiste 'Hola' ");
+
+num1 = parseInt(prompt("Ingrese un numero:"));
+
+if (num1 >= 10 && num1 <= 50) alert("El numero ingresado está entre 10 y 50");
